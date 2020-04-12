05d926491f900c51fb850084c24049dc271b2dba
Matt <mjkern@andrew.cmu.edu>
2020-04-11 21:10:01 -0400

    trying to refactor but broke js

--- mms.js
+++ mms.js
@@ -1,59 +1,86 @@
 // domain of the awesome table iframe that we will be messaging
 var domain = 'https://view-awesome-table.com/';
 
-function comsInit() {
-    setTimeout(function() {
-    
-        // make sure the awesome table exists and check back later if needed
-        if (document.querySelectorAll('iframe[data-type="AwesomeTableView"]').length < 1) {
-            console.log("too soon");
-            comsInit();
-            return;
-        }
-        
-        iframe = document.querySelectorAll('iframe[data-type="AwesomeTableView"]')[0].contentWindow;
-        
-        //listen to holla back
-        var gotResponse = false;
-        window.addEventListener('message',function(event) {
-            if(event.origin !== 'https://view-awesome-table.com') return;
-            if(event.data.type !== 'initResponse') {
-                console.log("bad response");
-                return;
-            }
-            console.log('received response:  ',event.data);
-            console.log(event.origin);
-            gotResponse=true;
-        },false);
-
-        
-        // initial message
-        function sendInitialMessage(iframe) {
-            google.script.run // this could all be more efficient
-                .withSuccessHandler(function (accessibleLinkData) {
-                    console.log(accessibleLinkData);
-                    var body = 'Hello!  The time is: ' + (new Date().getTime());
-                    var message = {
-                        "type": "initMessage",
-                        "body": body,
-                        "accessibleLinkData": accessibleLinkData
-                    };
-                    console.log('blog.local:  sending message: ' + 'message');
-                    iframe.postMessage(message,domain); //send the message and target URI
-                    setTimeout(function () {
-                        if (!gotResponse) {
-                            sendInitialMessage(iframe);
-                        }
-                    }, 1000);
-                })
-                .withFailureHandler(function (error) {
-                    console.log(error);
-                })
-                .accessibleEditLinks();
-        }
-        sendInitialMessage(iframe);
-
-    }, 10);
-    
+function doSlighlyLater(f) {
+    setTimeout(f, 1000);
 }
-comsInit();
+
+function doWhen(f, p) {
+    if (p()) {
+        f();
+    } else {
+        doSlighlyLater(doWhen(f, p));
+    }
+}
+
+function doUntil(f, p) {
+    if (!p()) {
+        f();
+        doSlighlyLater(doUntil(f, p));
+    }
+}
+
+function doWhenBoth(p1, p2, f) {
+    if (p1() && p2()) {
+        f();
+    } else {
+        doSlighlyLater(doWhenBoth(p1, p2, f));
+    }
+}
+
+//listen to holla back
+var gotResponse = false;
+window.addEventListener('message',function(event) {
+    if(event.origin !== domain) return;
+    if(event.data.type !== 'initResponse') {
+        console.log("bad response");
+        return;
+    }
+    console.log('received response:  ',event.data);
+    console.log(event.origin);
+    gotResponse=true;
+},false);
+
+function awesomeTableExists() {
+    return document.querySelectorAll('iframe[data-type="AwesomeTableView"]').length > 0;
+}
+
+accessInfo = null;
+function getAccessInfo() {
+    google.scrip.run
+        .withSuccessHandler(function(accessInfoResult) {
+            accessInfo = accessInfoResult;
+        })
+        .withSuccessHandler(function(error) {
+            console.log(error);
+            doSlighlyLater(getAccessInfo);
+        })
+        .accessibleEditLinks();
+}
+function accessInfoRecieved() {
+    return accessInfo !== null;
+}
+
+function awesomeTableConfirmed() {
+    return gotResponse;
+}
+
+function sendInitialMessage(iframe) {
+    var body = 'Hello!  The time is: ' + (new Date().getTime());
+    var message = {
+        "type": "initMessage",
+        "body": body,
+        "accessibleLinkData": accessInfo
+    };
+    console.log('blog.local:  sending message: ' + 'message');
+    iframe.postMessage(message,domain); //send the message and target URI
+}
+
+function sendDataToAwesomeTable() {
+    iframe = document.querySelectorAll('iframe[data-type="AwesomeTableView"]')[0].contentWindow;
+    sendInitialMessage(iframe);
+}
+
+doWhenBoth(awesomeTableExists, accessInfoRecieved, function() { 
+    doUntil(sendDataToAwesomeTable, awesomeTableConfirmed);
+});
