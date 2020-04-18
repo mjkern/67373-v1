8615338528e819478e9917c546986944acd7c55a
Matt <mjkern@andrew.cmu.edu>
2020-04-18 12:57:04 -0400

    refactored code now works

--- mms.js
+++ mms.js
@@ -1,5 +1,5 @@
-// domain of the awesome table iframe that we will be messaging
-var domain = 'https://view-awesome-table.com/';
+// for sending messages to the awesome table
+var domain = 'https://view-awesome-table.com';
 
 function doSlightlyLater(f) {
     setTimeout(f, 1000);
@@ -31,6 +31,8 @@ function doWhenBoth(p1, p2, f) {
 //listen to holla back
 var gotResponse = false;
 window.addEventListener('message',function(event) {
+    console.log("got a response before checking origin");
+    console.log(event.origin);
     if(event.origin !== domain) return;
     if(event.data.type !== 'initResponse') {
         console.log("bad response");
@@ -48,7 +50,7 @@ function awesomeTableExists() {
 accessInfo = null;
 function getAccessInfo() {
     console.log("getting access info");
-    google.scrip.run
+    google.script.run
         .withSuccessHandler(function(accessInfoResult) {
             console.log("got access info");
             accessInfo = accessInfoResult;
@@ -74,7 +76,7 @@ function sendInitialMessage(iframe) {
         "body": body,
         "accessibleLinkData": accessInfo
     };
-    console.log('blog.local:  sending message: ' + 'message');
+    console.log('blog.local:  sending message: ' + message);
     iframe.postMessage(message,domain); //send the message and target URI
 }
 
